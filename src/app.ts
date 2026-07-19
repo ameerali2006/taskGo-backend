885d421105aee812fc55d596fc2a2abf136bf557
Ameer Ali <ameerali10b@gmail.com>
2026-07-19 16:36:31 +0530

    Fix production build: move @types to dependencies and add ms package

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,17 @@ import cookieParser from "cookie-parser";
 import router from "./routes";
 const app = express();
 
+const allowedOrigins = process.env.CLIENT_URL || ["http://localhost:5173"];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin:allowedOrigins,
   credentials: true,
 }));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (_req, res) => {
+app.get("/", (_req: express.Request, res: express.Response) => {
   res.send("Task Management API Running...");
 });
 app.use("/api",router)
